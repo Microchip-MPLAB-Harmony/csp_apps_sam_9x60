0e8262e8c7ab330762ff228cf973c9f295f6a9f2
Jignesh Moradiya <jignesh.moradiya@microchip.com>
2021-01-06 14:48:53 +0530

    [APPS] Added rtt_rtt_alarm_sam_9x60_ek demo
    MH3-45791

---
+++ apps/rtt/rtt_alarm/firmware/src/main.c
@@ -0,0 +1,107 @@
+/*******************************************************************************
+  Main Source File
+
+  Company:
+    Microchip Technology Inc.
+  
+  File Name:
+    main.c
+
+  Summary:
+    This file contains the "main" function for a project.
+
+  Description:
+    This file contains the "main" function for a project.  The
+    "main" function calls the "SYS_Initialize" function to initialize the state 
+    machines of all modules in the system
+ *******************************************************************************/
+
+// DOM-IGNORE-BEGIN
+/*******************************************************************************
+* Copyright (C) 2020 Microchip Technology Inc. and its subsidiaries.
+*
+* Subject to your compliance with these terms, you may use Microchip software
+* and any derivatives exclusively with Microchip products. It is your
+* responsibility to comply with third party license terms applicable to your
+* use of third party software (including open source software) that may
+* accompany Microchip software.
+*
+* THIS SOFTWARE IS SUPPLIED BY MICROCHIP "AS IS". NO WARRANTIES, WHETHER
+* EXPRESS, IMPLIED OR STATUTORY, APPLY TO THIS SOFTWARE, INCLUDING ANY IMPLIED
+* WARRANTIES OF NON-INFRINGEMENT, MERCHANTABILITY, AND FITNESS FOR A
+* PARTICULAR PURPOSE.
+*
+* IN NO EVENT WILL MICROCHIP BE LIABLE FOR ANY INDIRECT, SPECIAL, PUNITIVE,
+* INCIDENTAL OR CONSEQUENTIAL LOSS, DAMAGE, COST OR EXPENSE OF ANY KIND
+* WHATSOEVER RELATED TO THE SOFTWARE, HOWEVER CAUSED, EVEN IF MICROCHIP HAS
+* BEEN ADVISED OF THE POSSIBILITY OR THE DAMAGES ARE FORESEEABLE. TO THE
+* FULLEST EXTENT ALLOWED BY LAW, MICROCHIP'S TOTAL LIABILITY ON ALL CLAIMS IN
+* ANY WAY RELATED TO THIS SOFTWARE WILL NOT EXCEED THE AMOUNT OF FEES, IF ANY,
+* THAT YOU HAVE PAID DIRECTLY TO MICROCHIP FOR THIS SOFTWARE.
+*******************************************************************************/
+// DOM-IGNORE-END
+
+
+// *****************************************************************************
+// *****************************************************************************
+// Section: Included Files
+// *****************************************************************************
+// *****************************************************************************
+
+#include <stddef.h>                     // Defines NULL
+#include <stdbool.h>                    // Defines true
+#include <stdlib.h>                     // Defines EXIT_FAILURE
+#include "definitions.h"         		// SYS function prototypes
+
+static bool alarmTriggered = false;
+
+void RTT_Callback(RTT_INTERRUPT_TYPE type, uintptr_t context)
+{
+	if( type & RTT_ALARM )
+	{
+        alarmTriggered = true;
+        LED_GREEN_Toggle();
+	}
+}
+// *****************************************************************************
+// *****************************************************************************
+// Section: Main Entry Point
+// *****************************************************************************
+// *****************************************************************************
+
+int main ( void )
+{ 
+    uint32_t rtt_val = 0;
+
+    /* Initialize all modules */
+    SYS_Initialize ( NULL );
+
+    printf("\n\r---------------------------------------------------------");
+    printf("\n\r                    RTT Demo");
+    printf("\n\r---------------------------------------------------------\n\r");
+
+    RTT_CallbackRegister(RTT_Callback, (uintptr_t) NULL);
+    rtt_val =  RTT_TimerValueGet();   
+    printf("\n\rThe Alarm will Trigger at RTT value %ld\n\n\r", (rtt_val + 10));
+
+    RTT_AlarmValueSet((rtt_val + 10));
+    
+    while( true )
+    {
+        printf("RTT value is:   %ld\r", RTT_TimerValueGet());
+        if( alarmTriggered )
+        {
+            alarmTriggered = false;
+            printf("\n\rAlarm Triggered !!!!!!!!\n\r");
+        }
+    }
+
+    /* Execution should not come here during normal operation */
+
+    return ( EXIT_FAILURE );
+}
+
+
+/*******************************************************************************
+ End of File
+*/
