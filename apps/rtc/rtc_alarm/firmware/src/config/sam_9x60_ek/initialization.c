05855fcf2086e68f12b82d9029f833ff712df572
Syam Nair <syam.nair@microchip.com>
2021-01-28 02:56:31 +0530

    [RTC] Regenerated demo for rtc_rtc_alarm_sam_9x60_ek
    MH3-47735
    
    Signed-off-by: Syam Nair <syam.nair@microchip.com>

--- apps/rtc/rtc_alarm/firmware/src/config/sam_9x60_ek/initialization.c
+++ apps/rtc/rtc_alarm/firmware/src/config/sam_9x60_ek/initialization.c
@@ -174,6 +174,7 @@ static void STDIO_BufferModeSet(void)
 
 void SYS_Initialize ( void* data )
 {
+
 	SYSC_Disable( );
 
     STDIO_BufferModeSet();
@@ -188,7 +189,7 @@ void SYS_Initialize ( void* data )
 
     MMU_Initialize();
 
-    INT_Initialize();
+    AIC_INT_Initialize();
     
     /* Disable WDT   */
     WDT_REGS->WDT_MR = WDT_MR_WDDIS_Msk;
