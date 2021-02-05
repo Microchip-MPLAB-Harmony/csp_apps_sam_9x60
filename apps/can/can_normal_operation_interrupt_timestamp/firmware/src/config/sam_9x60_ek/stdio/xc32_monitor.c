92a4b4852b006baa319be84b28ae86f53157bcc4
Jignesh Moradiya <jignesh.moradiya@microchip.com>
2021-02-05 12:40:28 +0530

    [CAN] Regenerated demo for can_can_normal_operation_interrupt_timestamp_sam_9x60_ek
    MH3-47690
    
    Signed-off-by: Jignesh Moradiya <jignesh.moradiya@microchip.com>

--- apps/can/can_normal_operation_interrupt_timestamp/firmware/src/config/sam_9x60_ek/stdio/xc32_monitor.c
+++ apps/can/can_normal_operation_interrupt_timestamp/firmware/src/config/sam_9x60_ek/stdio/xc32_monitor.c
@@ -48,16 +48,22 @@ void _mon_putc(char c);
 
 int _mon_getc(int canblock)
 {
-   volatile int c = 0;
-   while(DBGU_Read((void*)&c, 1) != true);
+   int c = 0;
+   bool success = false;
+   (void)canblock;
+   do
+   {
+       success = DBGU_Read(&c, 1);                
+   }while( !success);
    return c;
 }
 
 void _mon_putc(char c)
 {
-   uint8_t size = 0;
+   bool success = false;
    do
    {
-       size = DBGU_Write((void*)&c, 1);
-   }while (size != 1);
+       success = DBGU_Write(&c, 1);
+   }while (!success);
 }
+
