af7ab1aad224aca2c28e1169f81f5ef742272214
Syam Nair <syam.nair@microchip.com>
2021-08-04 11:23:02 +0530

    [ADC] Regenerated demo for adc_adc_automatic_window_comparison_sam_9x60_ek_iar
    MH3-57682
    
    Signed-off-by: Syam Nair <syam.nair@microchip.com>

--- apps/adc/adc_automatic_window_comparison/firmware/src/config/sam_9x60_ek_iar/peripheral/adc/plib_adc.h
+++ apps/adc/adc_automatic_window_comparison/firmware/src/config/sam_9x60_ek_iar/peripheral/adc/plib_adc.h
@@ -110,6 +110,15 @@ void ADC_ComparisonWindowSet(uint16_t lowThreshold, uint16_t highThreshold);
 bool ADC_ComparisonEventResultIsReady(void);
 
 void ADC_ComparisonRestart(void);
+
+void ADC_SleepModeEnable(void);
+
+void ADC_SleepModeDisable(void);
+
+void ADC_FastWakeupEnable(void);
+
+void ADC_FastWakeupDisable(void);
+
 // *****************************************************************************
 
 // DOM-IGNORE-BEGIN
