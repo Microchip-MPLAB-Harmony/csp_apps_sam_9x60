2459829e39a29192cb6c39e2d2d298a540ad371c
Jignesh Moradiya <Jignesh.Moradiya@microchip.com>
2024-10-28 11:47:36 +0530

    [RTT] Regenerated demo for rtt__rtt_periodic_timeout__firmware__sam_9x60_curiosity.X
    MH3-101140

--- apps/rtt/rtt_periodic_timeout/firmware/src/config/sam_9x60_curiosity/peripheral/pio/plib_pio.h
+++ apps/rtt/rtt_periodic_timeout/firmware/src/config/sam_9x60_curiosity/peripheral/pio/plib_pio.h
@@ -63,30 +63,30 @@
 
 
 /*** Macros for LED_GREEN pin ***/
-#define LED_GREEN_Set()               (PIOD_REGS->PIO_SODR = (1<<19))
-#define LED_GREEN_Clear()             (PIOD_REGS->PIO_CODR = (1<<19))
-#define LED_GREEN_Toggle()            (PIOD_REGS->PIO_ODSR ^= (1<<19))
-#define LED_GREEN_OutputEnable()      (PIOD_REGS->PIO_OER = (1<<19))
-#define LED_GREEN_InputEnable()       (PIOD_REGS->PIO_ODR = (1<<19))
-#define LED_GREEN_Get()               ((PIOD_REGS->PIO_PDSR >> 19) & 0x1)
+#define LED_GREEN_Set()               (PIOD_REGS->PIO_SODR = ((uint32_t)1U<<19U))
+#define LED_GREEN_Clear()             (PIOD_REGS->PIO_CODR = ((uint32_t)1U<<19U))
+#define LED_GREEN_Toggle()            (PIOD_REGS->PIO_ODSR ^= ((uint32_t)1U<<19U))
+#define LED_GREEN_OutputEnable()      (PIOD_REGS->PIO_OER = ((uint32_t)1U<<19U))
+#define LED_GREEN_InputEnable()       (PIOD_REGS->PIO_ODR = ((uint32_t)1U<<19U))
+#define LED_GREEN_Get()               ((PIOD_REGS->PIO_PDSR >> 19U) & 0x1U)
 #define LED_GREEN_PIN                  PIO_PIN_PD19
 
 /*** Macros for LED pin ***/
-#define LED_Set()               (PIOD_REGS->PIO_SODR = (1<<21))
-#define LED_Clear()             (PIOD_REGS->PIO_CODR = (1<<21))
-#define LED_Toggle()            (PIOD_REGS->PIO_ODSR ^= (1<<21))
-#define LED_OutputEnable()      (PIOD_REGS->PIO_OER = (1<<21))
-#define LED_InputEnable()       (PIOD_REGS->PIO_ODR = (1<<21))
-#define LED_Get()               ((PIOD_REGS->PIO_PDSR >> 21) & 0x1)
+#define LED_Set()               (PIOD_REGS->PIO_SODR = ((uint32_t)1U<<21U))
+#define LED_Clear()             (PIOD_REGS->PIO_CODR = ((uint32_t)1U<<21U))
+#define LED_Toggle()            (PIOD_REGS->PIO_ODSR ^= ((uint32_t)1U<<21U))
+#define LED_OutputEnable()      (PIOD_REGS->PIO_OER = ((uint32_t)1U<<21U))
+#define LED_InputEnable()       (PIOD_REGS->PIO_ODR = ((uint32_t)1U<<21U))
+#define LED_Get()               ((PIOD_REGS->PIO_PDSR >> 21U) & 0x1U)
 #define LED_PIN                  PIO_PIN_PD21
 
 /*** Macros for LED_RED pin ***/
-#define LED_RED_Set()               (PIOD_REGS->PIO_SODR = (1<<17))
-#define LED_RED_Clear()             (PIOD_REGS->PIO_CODR = (1<<17))
-#define LED_RED_Toggle()            (PIOD_REGS->PIO_ODSR ^= (1<<17))
-#define LED_RED_OutputEnable()      (PIOD_REGS->PIO_OER = (1<<17))
-#define LED_RED_InputEnable()       (PIOD_REGS->PIO_ODR = (1<<17))
-#define LED_RED_Get()               ((PIOD_REGS->PIO_PDSR >> 17) & 0x1)
+#define LED_RED_Set()               (PIOD_REGS->PIO_SODR = ((uint32_t)1U<<17U))
+#define LED_RED_Clear()             (PIOD_REGS->PIO_CODR = ((uint32_t)1U<<17U))
+#define LED_RED_Toggle()            (PIOD_REGS->PIO_ODSR ^= ((uint32_t)1U<<17U))
+#define LED_RED_OutputEnable()      (PIOD_REGS->PIO_OER = ((uint32_t)1U<<17U))
+#define LED_RED_InputEnable()       (PIOD_REGS->PIO_ODR = ((uint32_t)1U<<17U))
+#define LED_RED_Get()               ((PIOD_REGS->PIO_PDSR >> 17U) & 0x1U)
 #define LED_RED_PIN                  PIO_PIN_PD17
 
 
