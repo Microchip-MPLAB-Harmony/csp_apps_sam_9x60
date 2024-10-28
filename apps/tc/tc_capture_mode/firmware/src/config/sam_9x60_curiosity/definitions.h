1b2f831c420728e29c34070ac7450a0a2e7bd195
Jignesh Moradiya <Jignesh.Moradiya@microchip.com>
2024-10-28 11:41:04 +0530

    [TC] Regenerated demo for tc__tc_capture_mode__firmware__sam_9x60_curiosity.X
    MH3-101142

--- apps/tc/tc_capture_mode/firmware/src/config/sam_9x60_curiosity/definitions.h
+++ apps/tc/tc_capture_mode/firmware/src/config/sam_9x60_curiosity/definitions.h
@@ -53,8 +53,8 @@
 #include "peripheral/clk/plib_clk.h"
 #include "peripheral/pio/plib_pio.h"
 #include "peripheral/aic/plib_aic.h"
-#include "peripheral/dbgu/plib_dbgu.h"
 #include "peripheral/tc/plib_tc0.h"
+#include "peripheral/dbgu/plib_dbgu.h"
 #include "peripheral/pwm/plib_pwm.h"
 
 // DOM-IGNORE-BEGIN
@@ -66,13 +66,13 @@ extern "C" {
 // DOM-IGNORE-END
 
 /* Device Information */
-#define DEVICE_NAME			 "SAM9X60D1G"
-#define DEVICE_ARCH			 "ARM926EJ-S"
-#define DEVICE_FAMILY		 "SAM9"
-#define DEVICE_SERIES		 "SAM9X"
+#define DEVICE_NAME          "SAM9X60D1G"
+#define DEVICE_ARCH          "ARM926EJ-S"
+#define DEVICE_FAMILY        "SAM9"
+#define DEVICE_SERIES        "SAM9X6"
 
 /* CPU clock frequency */
-#define CPU_CLOCK_FREQUENCY 600000000
+#define CPU_CLOCK_FREQUENCY 600000000U
 
 // *****************************************************************************
 // *****************************************************************************
